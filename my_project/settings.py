dd0f783399ee7e9dc574beb28034c7b4426ac5be
Manveasna007 <manveasna1994@gmail.com>
2025-08-24 16:19:03 +0700

    Added necesary file  for deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-*qz^z@_yi0becfq4tralq1p4cfms-nrwmo^hx=^6%%9f@o4t+c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-weatherapp-railway-production.up.railway.app','localhost','127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https:django-weatherapp-railway-production.up.railway.app']
 
 # Application definition
 
